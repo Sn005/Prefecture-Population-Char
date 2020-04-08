24d3d05a41a2515ce31fef390cde574490c7c56f
Sn005 <living-inthe-junktown@hotmail.co.jp>
2020-04-08 10:09:28 +0900

    add: resas-api接続用のコード追加

---
+++ src/config/app.ts
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const RESAS_API_KEY = process.env.RESAS_API_KEY
---
+++ src/plugins/axios.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { RESAS_API_KEY } from "../config/app";
+const defaultConfig: AxiosRequestConfig = {
+  baseURL: "https://opendata.resas-portal.go.jp/api/v1/",
+  timeout: 7000,
+  headers: { "X-API-KEY": RESAS_API_KEY },
+};
+export const axiosInstance = axios.create(defaultConfig);
