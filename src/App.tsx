2c9357c2da83fbd4ebdff4e995f1ca939179ccb3
Sn005 <living-inthe-junktown@hotmail.co.jp>
2020-04-11 15:22:52 +0900

    reset.css追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React, { FC } from "react";
+import { Reset } from "styled-reset";
+
 import { Home } from "./components/pages/Home";
-const App: FC = () => <Home />;
+const App: FC = () => (
+  <>
+    <Reset />
+    <Home />
+  </>
+);
 
 export default App;
